c8236da19ae944ca54498719fed548042ff66793
Filipe Costa <116469491+c0sta90@users.noreply.github.com>
2024-01-03 12:28:58 -0100

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+int main ()
+// {
+//     int a = 5;
+//     int b = 5;
+//     int c = 2;
+//     int d = 1;
+//     int resultado = a%c; // o resto
+//     cout << resultado << endl;
+//     return 0;
+// }
