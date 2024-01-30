005f00e00224b758798b8401b208a2b6c6d74c7f
Costa <filipe.rocha.costa12@gmail.com>
2024-01-30 10:18:40 -0100

    -Update ex.cpp

--- ex.cpp
+++ ex.cpp
@@ -1,28 +1,28 @@
-#include <iostream>
-using namespace std;
 //Crie uma função capaz de ler os dados de uma matriz quadrada de inteiros. Ao final da leitura o programa deverá imprimir o número de linha que contem o menor dentro de todos os números lidos
+#include <iostream>
+#include <climits>
+using namespace std;
 
-// int array[3][3] = {{5,9,9},{8,9,2},{3,1,5}};
-// int menor = 0;
-// int linha = 0;
+int array[3][3] = {{5,9,9},{8,9,2},{3,1,5}};
+int menor = INT_MAX;
+int linha = 0;
 
-// void lerMatriz(int array[3][3]){
-//     for(int i = 0; i < 3; i++){
-//         for(int j = 0; j < 3; j++){
-//             cout << array[i][j] << " ";
-//             if(array[i][j] < menor){
-//                 menor = array[i][j];
-//                 linha = i;
-//             }
-//         }
-//         cout << endl;
-//     }
-//     cout << "Menor: " << menor << endl;
-//     cout << "Linha: " << linha << endl;
-// }
+void lerMatriz(int array[3][3]){
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            cout << array[i][j] << " ";
+            if(array[i][j] < menor){
+                menor = array[i][j];
+                linha = i;
+            }
+        }
+        cout << endl;
+    }
+    cout << "Menor: " << menor << endl;
+    cout << "Linha: " << linha << endl;
+}
 
-// int main(){
-//     lerMatriz(array);
-//     return 0;
-// }
-
+int main(){
+    lerMatriz(array);
+    return 0;
+}
