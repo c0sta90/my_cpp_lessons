e1a6aa7494491c45d4faf5b7bd0694bb2ef6b878
Costa <filipe.rocha.costa12@gmail.com>
2024-02-16 10:33:44 -0100

    update

---
+++ C++/cilofor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int a = 3;
+int b = 4;
+int c = 5;
+
+int main(){
+    if (a > b || b < c){
+        cout <<"é verdade" <<endl;
+    }else{
+        cout << "é mentira" << endl;
+    
+    }
+    return 0;
+}
+
+& // parametros de obrigadorio
