bb1296a5049f9d3e38c609e8be33308c15c1d9dd
Costa <filipe.rocha.costa12@gmail.com>
2024-03-01 10:07:08 -0100

    Update

--- C++/trabalho_aula/teste.cpp
+++ C++/trabalho_aula/teste.cpp
@@ -7,7 +7,7 @@ double O_MEU_NUMERO = 1010.5; // num reais
 char O_MEU_CARACTER = 'a'; // CARACTERES
 string O_MEU_TEXTO = "ISTO É UM TEXTO"; // TEXTO
 const double PI = 3.1415;
-bool O_MEU_BOLEANO = true;
+bool O_MEU_BOLEANO = true; //
 int x = 2, z =3, Y = 14;
 
 int a=2, b=3;
