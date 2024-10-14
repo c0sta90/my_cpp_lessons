e396e8fa3be394ffc94b7490c01a01576b5be62c
Filipe Costa <filipe.rocha.costa12@gmail.com>
2024-10-14 17:30:36 +0000

    Update

---
+++ 2º ano/fanta.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+class Refrigerante {
+private:
+string nome;
+int quantidade;
+int data;
+float preco;
+bool state;
+
+// ------------------------------------------------------------------------ //
+    void abrirRefri(){
+        if(state){
+            cout << "Estás a abrir a Fanta" << endl;
+        }
+        else{
+            cout << "A Fanta já estava aberta tolo" << endl;
+            this->state = true;
+        }
+    }
+
+    void beber(){
+    if(state){
+        cout << "A beber a fanta ..." << endl;
+    }
+    else{
+        cout << "A Fanta já está vazia!" << endl;
+        this->state = true;
+    }
+    };
+
+    void lixo(){
+        if (state)
+        cout << "Queres mesmo colocar a Fata cheia no lixo?" << endl;
+    }
+
+// ------------------------------------------------------------------------ //
+
+public:
+
+
+void Executar (int opt){
+            switch(opt){
+                case 1:
+                if(state){
+                    abrirRefri();
+                }else{
+                    cout << " " << endl;
+                }
+                case 2:
+                if(state){
+                    beber();
+                }else{
+                    cout << " " << endl;
+                }
+                case 3:
+                if(state){
+                    lixo();
+                }else{
+                    cout << " " << endl;
+                }
+            }
+        }
+};
---
+++ 2º ano/objetos.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Carros{
+public:
+    int velocidade;
+    int velocidadeMax;
+    string nome;
+    void insere(string x, int y);
+    void mostra();
+
+};
+
+int main(){
+
+    Carros carro1;
+
+
+    return 0;
+}
