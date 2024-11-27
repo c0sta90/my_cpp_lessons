81f2344dd81693c31ed9eafcd4b3a4be07bfcc33
Filipe Costa <filipe.rocha.costa12@gmail.com>
2024-11-27 12:20:47 -0100

    Update

---
+++ 2º ano/teste/teste.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "teste.h"
+
+using namespace std;
+
+void soma (int x, int y) {
+    cout << x + y << endl;
+}
---
+++ 2º ano/teste/teste.h
@@ -0,0 +1,7 @@
+#ifndef TESTE_H
+#define TESTE_H
+
+void soma(int x, int y);
+
+
+#endif // TESTE_H
