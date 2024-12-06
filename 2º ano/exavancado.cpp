cfff519a0e15431e82ed4ab6641ff662ee0ea55e
Filipe Costa <filipe.rocha.costa12@gmail.com>
2024-12-06 10:10:27 -0100

    Update

---
+++ 2º ano/exavancado.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+void matriz (int dim){
+
+    system("clear");
+
+    for (int i=0; i<= dim; i++){
+        for (int j=0; j<= dim; j++){
+            cout << setw(4) << "A"
+        }
+    }
+}
