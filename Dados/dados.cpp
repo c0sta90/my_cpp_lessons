23d1259cbdaf2b93db3a72c3977328be63179131
Filipe Costa <filipe.rocha.costa12@gmail.com>
2024-12-18 16:17:34 -0100

    Update

---
+++ Dados/dados.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void menu(){
+    system("clear");
+    cout<< "               Bem-Vindo ao Dado eletrónico" << endl;
+    cout<< " Programado em C/C++ Estruturas básicas e Conceitos Fundamentais" << endl;
+    cout<< "                        Enta-2024"<<endl;
+}
+
+int main(){
+
+    menu();
+}
