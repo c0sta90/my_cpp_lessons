2fd3c7e2cd1c12958cdc9d41b24eb8e328bf5ffa
Filipe Costa <filipe.rocha.costa12@gmail.com>
2024-12-18 17:01:34 -0100

    Update

--- Dados/dados.cpp
+++ Dados/dados.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void menu(){
+void cabeca(){
     system("clear");
     cout<< "               Bem-Vindo ao Dado eletrónico" << endl;
     cout<< " Programado em C/C++ Estruturas básicas e Conceitos Fundamentais" << endl;
     cout<< "                        Enta-2024"<<endl;
 }
 
+void numDado(){
+    srand(time(NULL));
+    int dado = rand()%6+1;
+    cout << dado << endl;
+}
+
+if (numDado == 1)
+{
+    cout << "" <<endl;
+    cout << "" <<endl;
+}
+
 int main(){
 
-    menu();
 }
